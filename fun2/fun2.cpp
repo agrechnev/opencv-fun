35f2a42feb235b3b47185114f8e686b1c20af468
agrechnev <shrike4625@gmail.com>
2017-08-31 08:36:40 +0300

    New fun2

--- fun2/fun2.cpp
+++ fun2/fun2.cpp
@@ -7,58 +7,86 @@
 #include "opencv2/opencv.hpp"
 
 
+using namespace std;
+using namespace cv;
+
+RNG rng(time(NULL));
+
 //--------------------------------------------
-static void process(cv::Mat & m, const std::vector<uchar> & tab){
-    using namespace std;
-    using namespace cv;
+static void fatal(string s){
+    cerr << s << endl;
+    exit(-1);
+}
+//--------------------------------------------
+static void process1(Mat & m, const uchar * const table){
+    int nCh = m.channels();
+    int ny = m.rows;
+    int nx = m.cols*nCh;
 
-    CV_Assert(m.depth() == CV_8U);
-    CV_Assert(tab.size() >= 256);
+    if (m.isContinuous()) {
+        nx *= ny;
+        ny = 1;
+    }
 
-    int ch = m.channels();
-    int ro = m.rows;
-    int co = m.cols;
+    uchar *p;
+    for (int iy = 0; iy < ny; ++iy){
+        p = m.ptr<uchar>(iy);
+        for (int ix = 0; ix < nx; ++ix)
+            p[ix] = table[p[ix]];
+    }
+}
+//--------------------------------------------
+static void process2(Mat & m, const uchar * const table){
+    CV_Assert(m.channels() == 3);
 
-    for (int i = 0; i < ro; i++)
-        for (int j = 0; j < co; j++)
-            swap(m.at<Vec3b>(i, j)[1], m.at<Vec3b>(i, j)[2]);
+    MatIterator_<Vec3b> it, end;
+    for (it = m.begin<Vec3b>(), end = m.end<Vec3b>(); it != end; ++it)
+        for (int c = 0; c<3; ++c)
+            (*it)[c] = table[(*it)[c]];
+}
+//--------------------------------------------
+static void process3(Mat & m, const uchar * const table){
+    CV_Assert(m.channels() == 3);
+    int ny = m.rows;
+    int nx = m.cols;
+
+    for (int iy = 0; iy < ny; ++iy)
+        for (int ix = 0; ix < nx; ++ix)
+            for (int c = 0; c<3; ++c)
+                m.at<Vec3b>(iy, ix)[c] = table[m.at<Vec3b>(iy, ix)[c]];
 
 }
 //--------------------------------------------
-
-int main(int arc, char** argv)
+int main(int argc, char** argv)
 {
-    using namespace std;
-    using namespace cv;
 
     cout << "Handmaiden's Love" << endl;
 
-    VideoCapture cap(CV_CAP_ANY); // open the default camera
+    constexpr int divideWith = 50;
 
-    // Create the table
+    uchar table[256];
+    Mat lut(1, 256, CV_8U);
+    uchar * p =lut.ptr();
 
-    constexpr int RUDE = 50;
-    vector<uchar> table(256);
-    for (int i = 0; i <= 255; ++i)
-        table[i] = (uchar)((i/RUDE)*RUDE);
+    for (int i=0; i<256; ++i)
+        table[i] = p[i] = (uchar)(divideWith*(i/divideWith));
 
-    cout << "Handmaiden's War" << endl;
+    Mat m1 = imread("img/lena512.png");
+    CV_Assert(m1.depth() == CV_8U);
+//    cout << "m1.channels() = " << m1.channels() << endl;
 
-    namedWindow("frameOut");
-    Mat frameIn, frameOut;
-    for(;;)
-    {
-        cap.read(frameIn); // get a new frame from camera
+    Mat m2 = m1.clone();
 
-        frameOut = frameIn.clone();
+    int64 t1 = getTickCount();
+//    process3(m2, table);
+    LUT(m1, lut, m2);
+    int64 t2 = getTickCount();
+    double d = 1000.0*(t2-t1)/getTickFrequency();
+    cout << "d = " << d << endl;
 
-        process(frameOut, table);
-
-        imshow("frameOut", frameOut);
-
-        if ('q' == waitKey(1))
-            break;
-    }
+    imshow("m1", m1);
+    imshow("m2", m2);
+    waitKey(0);
 
     return 0;
 }
