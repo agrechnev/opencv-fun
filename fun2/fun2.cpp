5002abd8f4fc64d604066500ec9bf298e8df4d05
Oleksiy Grechnyev <shrike4625@gmail.com>
2019-09-19 15:36:48 +0300

    Fun with sobel

--- fun2/fun2.cpp
+++ fun2/fun2.cpp
@@ -8,23 +8,20 @@ using namespace cv;
 
 
 int main(int argc, char **argv) {
-    const string keys = "{help h usage ?||HELP MESSAGE}{name |Brianna|Your name}{@file|<none>|File name}";
-    CommandLineParser parser(argc, argv, keys);
-    parser.about("Brianna App !!!");
-    if (parser.has("help")) {
-        parser.printMessage();
-        return 0;
-    }
-    string name = parser.get<string>("name");
-    cout << "name = " << name << endl;
-    string file = parser.get<string>(0);
-    cout << "file = " << file << endl;
-
-    if (!parser.check()) {
-        parser.printErrors();
-        return 0;
-    }
-
+    // 1-pixel matrix
+    Mat m = Mat::zeros(Size(600, 200), CV_8U);
+    m.at<uchar>(100, 300) = 1;
+    
+    // Sobel grad
+    Mat sx, sy, s;
+    Sobel(m, sx, CV_64F , 1, 0);
+    Sobel(m, sy, CV_64F , 0, 1);
+    sqrt(sx.mul(sx) + sy.mul(sy), s);
+        
+    imshow("sx", sx);
+    imshow("sy", sy);
+    imshow("s", s > 0.001);
+    waitKey(0);
     return 0;
 }
 
