eccf265d9b6a4bf3132e098f0388a3f0658fe66b
Oleksiy Grechnyev <shrike4625@gmail.com>
2019-12-16 11:57:29 +0200

    Fun with parallel_for_

--- fun2/fun2.cpp
+++ fun2/fun2.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include <opencv2/opencv.hpp>
 
-using namespace std;
-using namespace cv;
-
-
 int main(int argc, char **argv) {
-    // 1-pixel matrix
-    Mat m = Mat::zeros(Size(600, 200), CV_8U);
-    m.at<uchar>(100, 300) = 1;
-    
-    // Sobel grad
-    Mat sx, sy, s;
-    Sobel(m, sx, CV_64F , 1, 0);
-    Sobel(m, sy, CV_64F , 0, 1);
-    sqrt(sx.mul(sx) + sy.mul(sy), s);
-        
-    imshow("sx", sx);
-    imshow("sy", sy);
-    imshow("s", s > 0.001);
-    waitKey(0);
+    using namespace std;
+    using namespace cv;
+
+    cout << "OpenCV version = " << CV_VERSION << endl;
+
+    parallel_for_(Range(0, 10), [](const Range &r) -> void {
+        ostringstream oss;
+        oss << "r = " << r.start << " : " << r.end << "\n";
+        cout << oss.str();
+    }, -1);
+
+    cout << endl;
+
     return 0;
 }
 
